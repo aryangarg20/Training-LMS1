eb98b99dc9a8b0322fbe40a2e919e5f2eb01e1f7
Aryan Garg <aryan.garg@ukg.com>
2024-08-08 10:13:55 +0530

    Initial Commit

---
+++ lms1/src/test/java/com/ukg/lms1/Lms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukg.lms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lms1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
